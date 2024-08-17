fdc16c50d01edc20da8dae9b549db1ecf300f6bc
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-08-17 13:48:22 +0300

    fixed library model importation

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,7 @@
 from typing import Any
 from django.shortcuts import render
-from .models import Book, Library
+from .models import Book
+from .models import Library
 from django.views.generic import DetailView
 
 #function based view to list all books
