27f26e8a8d54e7649e8ea0c583faa88fc36b28ac
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-08-17 16:25:17 +0300

    hope it works

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -70,8 +70,23 @@ def logoutview(request):
 
 
 def is_admin(user):
-    return user.is_authenticated and user.userprofile.role == 'Admin'
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+     return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+     return user.userprofile.role == 'Member'
+     
 
 @user_passes_test(is_admin)
-def Admin_view(request):
-    return render(request, 'admin_view.html')
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
