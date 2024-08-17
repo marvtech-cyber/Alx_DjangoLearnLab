30aa99f051483b46e0024a4b2bc2a42becef368d
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-08-17 15:00:43 +0300

    fixed logout url

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('libraries/<pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
-    path('logout/', views.login_view, name='logout')
+    path('logout/', LogoutView.as_view(template_name=", "LoginView.as_view(template_name='logout.html')),
 ]
 
