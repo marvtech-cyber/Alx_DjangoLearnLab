21830a150cb3dd81f43ea6ca8b55dfc3e104d339
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-08-17 16:37:28 +0300

    fix

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -82,7 +82,7 @@ def is_member(user):
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
+    return render(request, 'admin_view.html')
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
