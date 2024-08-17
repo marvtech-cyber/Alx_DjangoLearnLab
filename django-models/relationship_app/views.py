560b5c3be42b53213b10ecfcd533db480457f9e4
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-08-17 16:14:46 +0300

    final fix

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,6 +4,7 @@ from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import User
 
 
 #function based view to list all books
@@ -71,20 +72,6 @@ def logoutview(request):
 def is_admin(user):
     return user.is_authenticated and user.userprofile.role == 'Admin'
 
-def is_librarian(user):
-    return user.is_authenticated and user.userprofile.role == 'Librarian'
-
-def is_member(user):
-    return user.is_authenticated and user.userprofile.role == 'Member'
-
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'admin_view.html')
-
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'librarian_view.html')
-
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'member_view.html')
