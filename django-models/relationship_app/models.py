c95a1d18317705572db1f9298c1087273d3207a9
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-08-17 15:58:42 +0300

    fixed user model

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Author(models.Model):
@@ -29,18 +31,20 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
     
+ROLE_CHOICES = [
+    ('Admin', 'Admin'),
+    ('Librarian', 'Librarian'),
+    ('Member', 'Member'),
+]
+
 class UserProfile(models.Model):
-    role_choices = [('Admin','Admin'),
-                    ('Librarian', 'Librarian'),
-                    ('Member', 'Member'),
-                    ]
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=role_choices)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
 
-    def __str__(self):
-        return self.user.username
-    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
     
 
     
