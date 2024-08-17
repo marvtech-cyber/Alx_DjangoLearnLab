48bcf7906d5519429d6476bc2bbba7f1681a79bb
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-08-17 16:16:41 +0300

    fix

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -73,5 +73,5 @@ def is_admin(user):
     return user.is_authenticated and user.userprofile.role == 'Admin'
 
 @user_passes_test(is_admin)
-def admin_view(request):
+def Admin_view(request):
     return render(request, 'admin_view.html')
