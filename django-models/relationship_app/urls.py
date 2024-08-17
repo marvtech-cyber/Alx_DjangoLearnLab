0330eb473401f7a1d2cba6fb2044278f8c9f2471
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-08-17 15:06:02 +0300

    fixed login pattern

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('libraries/<pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('register/', views.register, name='register'),
     path('logout/', views.LogoutView.as_view(template_name="logout")),
-    path('login/', views.LogoutView.as_view(template_name="login")),
+    path('login/', views.LoginView.as_view(template_name="login")),
 ]
 
