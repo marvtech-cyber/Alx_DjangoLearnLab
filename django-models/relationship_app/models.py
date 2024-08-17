25140f0e9ae921df44433c0c29009d08217b1531
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-08-17 12:34:24 +0300

    added string return representation in classes

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -4,14 +4,26 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
     library = models.ManyToManyField(Book)
 
+    def __str__(self):
+        return self.name
+
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
