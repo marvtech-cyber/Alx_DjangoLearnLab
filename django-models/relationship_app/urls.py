0eb3699251cd78001332db513eb9e8fd6effbb04
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-08-17 14:54:31 +0300

    implemented user login register and logout

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,4 +6,8 @@ from. import views
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('libraries/<pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.login_view, name='logout')
 ]
+
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,5 @@
 from typing import Any
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
@@ -25,3 +25,42 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        
+        else:
+            form = UserCreationForm()
+            context = {'form: form'}
+
+        return render(request, 'relationship_app/register.html', context)
+    
+
+def login_view(request):
+    if request.method == 'POST':
+        form =AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+
+            else:
+                return "Invalid credentials"
+        else:
+            form = AuthenticationForm()
+        context = {'form':form}
+        return render(request, 'login.html', context)
+    
+def logout_view(request):
+    logout(request)
+    return redirect('login_view')
