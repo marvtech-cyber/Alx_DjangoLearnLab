90e6097f0d0642b38af2df969ce391f7981e4355
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-08-17 17:14:13 +0300

    task 4

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404, redirect
+
 
 # Create your models here.
 class Author(models.Model):
@@ -13,6 +16,14 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    
+    class Meta:
+        permissions = [
+            ('can_add_book', 'Can add a book'),
+            ('can_change_book', 'Can change a book'),
+            ('can_delete_book', 'Can delete a book'),
+        ]
 
     def __str__(self):
         return self.name
@@ -47,4 +58,34 @@ def create_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
     
 
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    # Implement adding a book here
+    if request.method == 'POST':
+        # Process form data
+        pass
+    else:
+        # Render form
+        pass
+    return render(request, 'add_book.html') 
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        # Process form data to update the book
+        pass
+    else:
+        # Render form with book data
+        pass
+    return render(request, 'edit_book.html', {'book': book})
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')  
+    else:
+        return render(request, 'delete_book.html', {'book': book})
     
