1d758c6bcd6dce49fc8a9c7384b23da935d8d242
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-08-17 17:18:44 +0300

    fixed url for add book and edit book

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.decorators import permission_required
-from django.shortcuts import render, get_object_or_404, redirect
+
 
 
 # Create your models here.
