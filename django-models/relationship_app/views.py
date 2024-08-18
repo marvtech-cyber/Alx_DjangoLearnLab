272a677da10b39250bbc165f936edbfc7d70e7b2
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-08-18 08:47:39 +0300

    fixed admin files

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -73,7 +73,7 @@ def logoutview(request):
 
 
 def is_admin(user):
-    return user.userprofile.role == 'Admin'
+    return user.is_authenticated and user.userprofile.role == 'Admin'
 
 def is_librarian(user):
      return user.userprofile.role == 'Librarian'
@@ -85,9 +85,9 @@ def is_member(user):
 def admin_check(user):
     return user.userprofile.role == 'Admin'
 
-@user_passes_test(admin_check)
+@user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'relashionship_app/admins.html')
 
 def member_check(user):
     return user.userprofile.role == 'Member'
