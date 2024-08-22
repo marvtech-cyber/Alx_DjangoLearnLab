d9f2ae057e49cac55d24169c58c356f14f899930
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-08-22 15:01:07 +0300

    fixed project location

--- advanced_features_and_security/bookshelf/admin.py
+++ advanced_features_and_security/bookshelf/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser
+from django.contrib.auth.admin import UserAdmin as CustomUserAdmin
 # Register your models here.
-
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_filter = ('title', 'author', 'publication_year')
-    search_fields = ('title', 'author', 'publication_year')
+    list_display = ('title','author', 'publication_year')
+    list_filter = ('title','author','publication_year')
+    search_fields = ('title','author')
 
-admin.site.register(Book, BookAdmin)
+class UserAdmin(CustomUserAdmin):
+    pass
+admin.site.register(CustomUser, CustomUserAdmin)
