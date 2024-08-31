5c534878859542de02fefe6b95b1d562e469fef2
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-08-31 09:49:17 +0300

    django rest token authentication setup

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import BookViewSet
+from .views import BookViewSet, CustomAuthToken
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'Book', BookViewSet)
 urlpatterns = [
+    path('api-token-auth/', CustomAuthToken.as_view()),
     path('api/', include(router.urls))
 
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 from .serializers import BookSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Book
 from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,context = {'request':request})
+        serializer.is_valid(raise_exception = True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        }, status=status.HTTP_200_OK)
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializers = BookSerializer
+
+    def get_permission(self):
+        if self.action == 'list':
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
