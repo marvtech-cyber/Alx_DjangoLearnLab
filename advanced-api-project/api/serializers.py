93ba1f6c58a14a03b4e7eacd6b66933c16a35ea9
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-09-08 12:31:21 +0300

    updated fields

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -32,4 +32,4 @@ class AuthorSerializer(serializers.ModelSerializer):
     books = BookSerializer(many = True, read_only = True)
     class Meta:
         model = Author
-        fields = ['name']
+        fields = ['name', 'books']
