280e6c1ecfe923ee291b0660ef52d9caebe168e2
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-09-11 15:20:19 +0300

    fixed urls for login

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('register/', views.register, name="register"),
     path('profile/', views.profile_view, name = 'profile'),
+     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/edit/', views.edit_profile_view, name = 'profile_edit'),
 ]
