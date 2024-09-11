76522ce3aeae6aee513ee20b791133fea014142c
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-09-11 12:48:30 +0300

    setup login and profile url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('register/', views.register, name="register")
+    path('register/', views.register, name="register"),
+    path('login/', views.login, name="login"),
+    path('profile/', views.profile, name="profile")
 ]
