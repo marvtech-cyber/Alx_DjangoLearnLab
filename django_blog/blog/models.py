0fc6c9491b2955f8c507aaa752dc4b9e3731a6da
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-09-12 15:22:25 +0300

    updated comment model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -20,4 +20,4 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete= models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add= True)
-    update_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
