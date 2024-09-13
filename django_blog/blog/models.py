7d6e030177585421519736f2918d31a014c5e833
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-09-13 11:26:45 +0300

    updated models

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,17 +2,29 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
 class Post(models.Model):
     title = models.CharField(max_length = 200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    tags = models.ManyToManyField('Tag', related_name='tag')
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
+
+    def __str__(self):
+        return self.title
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
+
+    def __str__(self):
+        return self.title
 
 
 #Comments model
@@ -23,5 +35,7 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add= True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
-class Tag(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    def __str__(self):
+        return self.title
+
+
