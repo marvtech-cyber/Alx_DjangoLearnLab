cc512176b1b58c38a61a1af57744c44103d50b33
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-09-16 17:29:14 +0300

    upated serializers structure

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,12 +5,40 @@ from rest_framework.authtoken.models import Token
 CustomUser = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the CustomUser model.
+    """
     class Meta:
         model = CustomUser
         fields = ('id', 'username', 'email', 'password', 'bio', 'profile_picture')
         extra_kwargs = {'password': {'write_only': True}}
 
+    def create(self, validated_data):
+        """
+        Create a new user instance.
+        """
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', '')
+        )
+        return user
+
 class TokenSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Token model.
+    """
+    token = serializers.CharField()
+
     class Meta:
         model = Token
         fields = ('key',)
+
+    def create(self, validated_data):
+        """
+        Create a new token instance.
+        """
+        token = Token.objects.create(user=self.context['request'].user)
+        return token
