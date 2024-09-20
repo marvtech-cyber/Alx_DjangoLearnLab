84d7fddf09c2c7059ac66c493e3d480a48173701
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-09-20 13:29:44 +0300

    like viewset

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet, FeedView
+from .views import PostViewSet, CommentViewSet, FeedView, LikeViewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -14,4 +14,6 @@ urlpatterns = [
     path('posts/<int:post_id>/comments/', CommentViewSet.as_view({'get': 'list', 'post': 'create'}), name='post-comments'),
     path('posts/<int:post_id>/like/', PostViewSet.as_view({'post': 'like', 'delete': 'unlike'}), name='post-like'),
     path('feed/', FeedView.as_view(), name='feed'),
+      path('posts/<int:pk>/like/', LikeViewSet.as_view({'post': 'create', 'delete': 'destroy'}), name='post-like'),
+    path('posts/<int:pk>/unlike/', LikeViewSet.as_view({'post': 'create', 'delete': 'destroy'}), name='post-unlike'),
 ]
